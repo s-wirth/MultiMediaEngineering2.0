fa758fe9b6c0137ad477c06a43b5556e03c76ce0
Sophie <a.sophiewirth@gmail.com>
2015-01-18 15:49:48 +0100

    beginning to include JS

---
+++ Scripts/javascript.js
@@ -0,0 +1,23 @@
+// jshint ignore: start
+
+window.addEventListener("load",init, false);
+
+function init() {
+	var loginBtn = document.getElementById("loginBtn");
+	var signupBtn = document.getElementById("signupBtn");
+	loginBtn.addEventListener("click", checkLogin, false);
+	signupBtn.addEventListener("click", checkSignup, false);
+}
+
+function checkLogin(e) {
+	var username = document.getElementById("login-name");
+	var pw = document.getElementById("login-pw");
+
+	if(username.value.indexOf('sophie') >= 0){
+		window.location.href = "profile.html";
+	}else window.location.href = "errorPage.html";
+}
+
+function checkSignup(e) {
+	window.location.href = "signUp.html";
+}
---
+++ Scripts/rescueJS.js
@@ -0,0 +1,29 @@
+// jshint ignore: start
+
+window.addEventListener("load",init, false);
+
+function init() {
+	var submit = document.getElementById("reset");
+	submit.addEventListener("click", submitMail, false);
+	setTimeout(timeout, 9000);
+
+}
+
+function submitMail() {
+	var mail = document.getElementById("mail");
+	if(mail.value == ""){
+		alert("Please enter an E-Mail adress.");
+	}else if(mail.value.indexOf('@') < 0 ){
+		alert("No valid E-Mail adress.");	
+	}else if(mail.value.indexOf('@beuth-hochschule.de') < 0 ){
+		alert("E-Mail must be registered at @beuth-hochschule.de");
+	}else {
+		alert("An E-Mail has been sent to you.");
+		window.location.href = 'index.html';
+	}
+}
+
+function timeout() { 
+	alert("Your session has expired.")
+	window.location.href = 'index.html';
+}
